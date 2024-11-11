0465c71e48c73cbfd591267ed9d1a89138f36859
chandhiyasuresh <161188438+chandhiyasuresh@users.noreply.github.com>
2024-11-11 18:24:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app = express();
 const port = 3000;
 
 // Your Twilio credentials
-const accountSid = 'AC6fa97b7019dd111118f934795dd4cb10';  // Replace with your Account SID
-const authToken = 'c55a099929fdf467aa872546f8df233c';    // Replace with your Auth Token
-const fromWhatsApp = 'whatsapp:+14155238886'; // Twilio WhatsApp-enabled number
-const toWhatsApp = 'whatsapp:+916385297091'; // Your WhatsApp number
+const accountSid = process.env.accountSid;  // Replace with your Account SID
+const authToken = process.env.authToken;    // Replace with your Auth Token
+const fromWhatsApp = process.env.fromWhatsApp; // Twilio WhatsApp-enabled number
+const toWhatsApp = process.env.toWhatsApp; // Your WhatsApp number
 
 // Twilio client
 const client = twilio(accountSid, authToken);
@@ -19,14 +19,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Endpoint to handle form submissions
 app.post('/submit-form', (req, res) => {
-  const { name, email, message } = req.body; // Assuming the form has fields 'name', 'email', and 'message'
+  const { name, college , email, phno , department , year  } = req.body; // Assuming the form has fields 'name', 'email', and 'message'
 
   // Construct the message content
   const messageContent = `
     New Form Submission:
     Name: ${name}
     Email: ${email}
-    Message: ${message}
+    college: ${college}
+    phoneNumber : ${phno}
+    Department: ${department}
+    Year: ${year}
   `;
 
   // Send WhatsApp message via Twilio
